ff4eed401024c0667d8dde7dc9712dc40f03ff7e
finnian-smith <finniansmith01@gmail.com>
2024-02-28 12:11:31 +0000

    Build modal dialog element

--- script.js
+++ script.js
@@ -5,21 +5,13 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return title + " by " + author + ", " + pages + " pages, " + read;
-  };
 }
 
 // Template Books
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "read");
-const book2 = new Book(
-  "Who Moved My Cheese?",
-  "Dr Spencer Johnson",
-  94,
-  "not read yet"
-);
-const book3 = new Book("Meditations", "Marcus Aurelius", 254, "not read yet");
-const book4 = new Book("Shoe Dog", "Phil Knight", 399, "read");
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const book2 = new Book("Who Moved My Cheese?", "Dr Spencer Johnson", 94, false);
+const book3 = new Book("Meditations", "Marcus Aurelius", 254, false);
+const book4 = new Book("Shoe Dog", "Phil Knight", 399, true);
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
@@ -31,16 +23,65 @@ addBookToLibrary(book2);
 addBookToLibrary(book3);
 addBookToLibrary(book4);
 
-function loopLibrary(myLibrary) {
+function displayBook(book) {
   const bookCards = document.querySelector(".book-cards");
 
-  myLibrary.forEach((book) => {
-    // make p -> div then create elements like h3, p, image etc. and append to div
-    const bookCard = document.createElement("p");
-    bookCard.innerText = book.title;
-    bookCard.classList.add("book-card");
-    bookCards.appendChild(bookCard);
-  });
+  // make p -> div then create elements like h3, p, image etc. and append to div
+  const bookCard = document.createElement("p");
+  bookCard.innerText = book.title;
+  bookCard.classList.add("book-card");
+  bookCards.appendChild(bookCard);
 }
 
-loopLibrary(myLibrary);
+myLibrary.forEach((book) => {
+  displayBook(book);
+});
+
+// MODAL
+const addButton = document.querySelector(".add-book-button");
+const bookDialog = document.querySelector(".book-dialog");
+const bookForm = document.querySelector(".book-form");
+const confirmAddButton = bookDialog.querySelector(".confirm-add-button");
+const backdrop = document.querySelector(".backdrop");
+
+// "Add Book" button opens the modal and applies backdrop
+addButton.addEventListener("click", () => {
+  bookDialog.showModal();
+  backdrop.style.display = "block";
+});
+
+// HANDLE SUBMIT - this can maybe be cleaned up?
+function captureBookData() {
+  const bookData = {
+    title: document.querySelector("#title").value,
+    author: document.querySelector("#author").value,
+    pages: document.querySelector("#pages").value,
+    read: document.querySelector("#read").checked,
+  };
+  return bookData;
+}
+
+function handleBookAddition(event) {
+  event.preventDefault();
+  const bookData = captureBookData();
+  const newBook = new Book(
+    bookData.title,
+    bookData.author,
+    bookData.pages,
+    bookData.read
+  );
+  addBookToLibrary(newBook);
+  console.log(newBook);
+  displayBook(newBook);
+  bookDialog.close();
+  bookForm.reset();
+  backdrop.style.display = "none";
+}
+
+confirmAddButton.addEventListener("click", handleBookAddition);
+
+// "Cancel" button closes the dialog
+bookDialog.addEventListener("close", (e) => {
+  backdrop.style.display = "none";
+  // maybe reset form here too?
+});
